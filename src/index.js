492d8fcbf93af43c30c75999542c745a9443894e
SergeySerzhan <s.sergey96@mail.ru>
2021-03-09 22:21:29 +0300

    Ended webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-console.log('Test');
-
+import "./style.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,57 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports= {
-  mode: 'development',
-  entry: './src/index.js',
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const devtool = function () {
+  if (isDev) return "source-map";
+  else "";
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./index.js",
   output: {
     filename: "bundle[contenthash].js",
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 4200,
+    hot: isDev,
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html',
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ],
+  devtool: devtool(),
+  module: {
+    rules: [
+      {
+        test: /s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
