e8d4ee6f90cd1546443f3a4025d7ea4cca5b6da0
SergeySerzhan <s.sergey96@mail.ru>
2021-03-12 15:06:06 +0300

    Begin team section

--- src/index.js
+++ src/index.js
@@ -10,33 +10,30 @@ const btnRight = document.querySelector(".slider__btn--right");
 // Transform translateX  origin
 let transX = 225;
 
-const slideLeft = function () {
-  transX = 225
-    ? slidesArr.forEach((card) => {
-        card.classList.add("end", "endleft");
-      })
-    : slidesArr.forEach((card) => {
-        card.classList.remove("end", "endleft");
-      });
-  transX += 112.5;
-  slidesArr.forEach(
-    (card) => (card.style.transform = `translateX(${transX}%)`)
-  );
-};
+const removeClass=function(clas) {
+  slidesArr.forEach(card=>card.classList.remove(clas));
+}
 
-const slideRight = function () {
-  transX = -225
-    ? slidesArr.forEach((card) => {
-        card.classList.add("end", "endright");
-      })
-    : slidesArr.forEach((card) => {
-        card.classList.remove("end", "endright");
-      });
-  transX -= 112.5;
+const slide = function(pos, clas, animTime, direction) {
+  if(transX===pos) {
+    slidesArr.forEach(card=>card.classList.add(clas));
+    setTimeout(removeClass, animTime*1000+200, clas);
+    return;
+  }
+
+  direction==='left' ? transX+=112.5 : transX-=112.5;
   slidesArr.forEach(
     (card) => (card.style.transform = `translateX(${transX}%)`)
   );
-};
+}
+
+const slideLeft = function() {
+  slide(225, "endleft", 0.5,"left");
+}
+
+const slideRight = function() {
+  slide(-225, "endright", 0.5,"right");
+}
 
 btnLeft.addEventListener("click", slideLeft);
 btnRight.addEventListener("click", slideRight);
