7cac4928e2899f6910237a204fecaa1b4fcae8a0
SergeySerzhan <s.sergey96@mail.ru>
2021-03-16 15:38:49 +0300

    Begin responsive

--- src/index.js
+++ src/index.js
@@ -37,3 +37,14 @@ const slideRight = function() {
 
 btnLeft.addEventListener("click", slideLeft);
 btnRight.addEventListener("click", slideRight);
+
+
+// MAP 
+
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+L.marker([51.505, -0.09]).addTo(mymap);
